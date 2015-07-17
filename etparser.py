b35e11f814a5d74e9dad7432bddb73dfbb7b0b95
Karl Cronburg <karl.cronburg@gmail.com>
2015-07-17 16:18:41 -0400

    parser helper functions, some comments for understanding simulator & solver

--- etparser.py
+++ etparser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 """
 Usage in python:
 
@@ -23,7 +24,8 @@ trace file format.
 import sys
 from pprint import pprint
 
-__all__ = ["parse_event", "EventIter"]
+__all__ = [ "parse_event", "EventIter", "is_valid_op", "is_heap_op"
+          , "is_heap_alloc_op", "is_method_op" ]
 
 # Guarantees tuple of length num returned, else die:
 def trysplit(line, num):
@@ -38,36 +40,27 @@ def trysplit(line, num):
 def dict_zip(line, keys):
   return dict(zip(keys, trysplit(line, len(keys))))
 
-# Allocation event parser:
-alloc_keys = ['objID', 'size', 'type', 'site', 'length', 'threadID']
-def alloc(typ):
-  def _alloc(l):
-    d = dict_zip(l, alloc_keys)
-    d.update({'allocType': typ})
-    return d
-  return _alloc
-
 # Generate event parser expecting the given keys:
 def event_parser(keys): return (lambda l: dict_zip(l, keys))
 
 # Fields for each type of event (to be used as dictionary keys):
+alloc_keys = ['objID', 'size', 'type', 'site', 'length', 'threadID']
 object_death_keys = ['objID'] # TODO: threadID, but it's not in '201.trace'
 update_field_keys = ['oldTargetID', 'objID', 'newTargetID', 'fieldID', 'threadID']
-method_entry_keys = ['methodID', 'receiverObjID', 'threadID']
-method_exit_keys = ['methodID', 'receiverObjID', 'threadID']
-exceptional_exit_keys = ['methodID', 'receiverObjID', 'exceptionID', 'threadID']
-exception_keys = ['methodID', 'receiverID', 'exceptionObjID', 'threadID']
-exception_handled_keys = ['methodID', 'receiverID', 'exceptionObjID', 'threadID']
-root_object_keys = ['rootObjID', 'threadID']
+method_entry_keys = ['objID', 'receiverObjID', 'threadID']
+method_exit_keys = ['objID', 'receiverObjID', 'threadID']
+exceptional_exit_keys = ['objID', 'receiverObjID', 'exceptionID', 'threadID']
+exception_keys = ['objID', 'receiverID', 'exceptionObjID', 'threadID']
+exception_handled_keys = ['objID', 'receiverID', 'exceptionObjID', 'threadID']
+root_object_keys = ['objID', 'threadID']
 
 # Map from 'ET Record Types' to corresponding parser function
 _parse_event = {
-    '#': lambda l: None
-  , 'A': alloc('A') # Array alloc
-  , 'I': alloc('I') # Initial heap alloc
-  , 'N': alloc('N') # Alloc via new byte code
-  , 'P': alloc('P') # Discovered after actual alloc
-  , 'V': alloc('V') # VM object
+    'A': event_parser(alloc_keys) # Array alloc
+  , 'I': event_parser(alloc_keys) # Initial heap alloc
+  , 'N': event_parser(alloc_keys) # Alloc via new byte code
+  , 'P': event_parser(alloc_keys) # Discovered after actual alloc
+  , 'V': event_parser(alloc_keys) # VM object
   , 'D': event_parser(object_death_keys)
   , 'U': event_parser(update_field_keys)
   , 'M': event_parser(method_entry_keys)
@@ -81,14 +74,16 @@ _parse_event = {
 # Returns a python dictionary parsed from the given ET event in string / line format:
 def parse_event(line):
   '''Parse the given line as an Elephant Tracks event.'''
-  if len(line) == 0:
+  if len(line) == 0 or line[0] == '#':
     return None
   try:
     parser = _parse_event[line[0]]
   except KeyError as e:
     print "Unknown event type for line '%s'" % (line,)
     exit(-1)
-  return parser(line[1:])
+  d = parser(line[1:])
+  d.update({'rectype': line[0]})
+  return d
 
 # Iterator over lines of an ET trace (e.g. over lines from a file stream object):
 class EventIter:
@@ -100,6 +95,12 @@ class EventIter:
     while d is None:
       d = parse_event(self.stream.next())
     return d
+
+# Event helper functions:
+def is_valid_op(op):      return op in _parse_event.keys()
+def is_heap_op(op):       return op in list('AINPVDU')
+def is_heap_alloc_op(op): return op in list('AINPV')
+def is_method_op(op):     return op in list('ME')
 
 if __name__ == '__main__':
   with open('test1.trace', 'r') as fp:
--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from etparse import is_valid_op, is_heap_alloc_op, is_heap_op, parse_line, \
-                    is_valid_version
+# -*- coding: utf-8 -*- 
+from etparser import is_heap_alloc_op, is_heap_op
+from etparser import parse_event as parse_line
 from pprint import PrettyPrinter
 from z3 import *
 pp = PrettyPrinter(indent=2)
@@ -22,13 +23,21 @@ start = Int('start')
 end = Int('end')
 t = Int('t')
 
-
+# interval_pointsat :: Int -> Int -> Int -> Int -> Bool
 interval_pointsat = Function('interval_pointsat', IntSort(), IntSort(), IntSort(), IntSort(), BoolSort())
+
+# instant_pointsat :: Int -> Int -> Int -> Bool
 instant_pointsat = Function('instant_pointsat', IntSort(), IntSort(), IntSort(), BoolSort())
 
+# type_function :: Int -> Int
 type_function = Function('type_function', IntSort(), IntSort())
 
-interval_to_instant = ForAll([x, y, start, end, t] , Implies( And(t >= start,  t <= end, interval_pointsat(x, y, start, end)), instant_pointsat(x, y, t))) 
+# ∀ (x,y,start,end,t), (  t >= start && t <= end
+#                      && interval_poinsat(x,y,start,end)
+#                      ) ⇒ instant_pointsat(x,y,t)
+_interval_condtn = And(t >= start, t <= end, interval_pointsat(x, y, start, end))
+_interval_implies = Implies(_interval_condtn, instant_pointsat(x, y, t))
+interval_to_instant = ForAll([x, y, start, end, t], _interval_implies)
 
 solver.add(interval_to_instant)
 
@@ -38,25 +47,26 @@ next_type_index = 0
 with open('test1.trace', 'r') as fp:
     for line in fp:
         d = parse_line(line)
-        obj_id = d['objId']
+        obj_id = d['objID']
         if is_heap_alloc_op(d['rectype']):
             i += 1
-	    ty = d['type']
-            heap[obj_id] =  ObjectModel(obj_id, ty)
+            ty = d['type']
+            heap[obj_id] = ObjectModel(obj_id, ty)
 
             if (ty not in ty2index):
                ty2index[ty] = next_type_index
                next_type_index += 1
             
-            solver.add(type_function(obj_id) == ty2index[ty])            	    
+            # Tell solver new heap alloc has the given type
+            solver.add(type_function(obj_id) == ty2index[ty])
  
         elif d['rectype'] == 'U':
             update_count += 1
             if obj_id > 0 and heap.has_key(obj_id):
                 o_model = heap[obj_id]
-                field_id = d['fieldId']
+                field_id = d['fieldID']
 
-                new_target = d['newTgtId']
+                new_target = d['newTargetID']
 
                 if (field_id in o_model.fields):
                     (creation_time, old_target) = o_model.fields[field_id]
@@ -85,7 +95,7 @@ for o_model in heap.values():
 # PointsAt(ObjectA, ObjectB, StartTime, EndTime) 
 # Type(Object, Ty)
 # ~(Exists x, y, z, t1
-#     s.t.  Type(LLN, y), Type(LLN, x), Type(LLN, z),  
+#     s.t.  Type(LLN, y), Type(LLN, x), Type(LLN, z),
 #            instant_pointsat(y, z, t) && instant_pointsat(x, z, t)) 
 
 test_object_index = ty2index["LTestObject;"]
@@ -95,8 +105,6 @@ solver.add(Exists([x,y,z,t], And([instant_pointsat(x, z, t),
                       type_function(x) == test_object_index,
                       type_function(y) == test_object_index,
                       type_function(z) == test_object_index])) )
-
-
 
 
 print solver.check()
