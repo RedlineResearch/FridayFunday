725065074d4993c67c10b897dadb6922fa702a1b
Diogenes Nunez <dan@dell26.eecs.tufts.edu>
2015-09-28 11:31:21 -0400

    Change the path to trace file to be an arg of the program rather than a pipe to prevent early termination of reader reading from stdin.

--- datalog/run_iris.sh
+++ datalog/run_iris.sh
@@ -12,6 +12,6 @@ else
     make all
     pushd ./src > /dev/null
     echo "Running program"
-    java -cp .:../lib/iris-0.60.jar:../lib/iris-app-0.60.jar:../lib/iris-parser-0.60.jar net.veroy.analysis.IrisTest01 < $TRACEFILE
+    java -cp .:../lib/iris-0.60.jar:../lib/iris-app-0.60.jar:../lib/iris-parser-0.60.jar net.veroy.analysis.IrisTest01 $TRACEFILE
     popd > /dev/null
 fi
