85864f4eddadaa62c5cab05c5c9fa0aafa18fb86
Diogenes Nunez <dan@dell26.eecs.tufts.edu>
2015-10-02 12:12:19 -0400

    Fixed an error where using a path that actually existed triggered the does-not-exist error.

--- datalog/run_iris.sh
+++ datalog/run_iris.sh
@@ -4,14 +4,15 @@ if [ -z $TRACEFILE ]
 then
     echo "Missing trace file. Add **absolute** path to trace file."
     echo "Usage: run_iris.sh /absolute/path/to/trace"
-elif [ -e $TRACEFILE ]
+elif [ ! -e $TRACEFILE ]
 then
-    echo "File pointed to by path does not exist"
+    echo "File referenced by ${TRACEFILE} does not exist"
     echo "Usage: run_iris.sh /absolute/path/to/trace"
 else
     make all
     pushd ./src > /dev/null
     echo "Running program"
+    # java -cp .:../lib/iris-0.60.jar:../lib/iris-app-0.60.jar:../lib/iris-parser-0.60.jar -Xms16G -Xmx64G -XX:-UseGCOverheadLimit net.veroy.analysis.IrisTest01 $TRACEFILE
     java -cp .:../lib/iris-0.60.jar:../lib/iris-app-0.60.jar:../lib/iris-parser-0.60.jar net.veroy.analysis.IrisTest01 $TRACEFILE
     popd > /dev/null
 fi
